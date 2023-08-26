2bb4e71483eaa6f5203bbeda7dde476679795eb6
ganesh8prasad1991 <ganesh8prasad1991@gmail.com>
2023-08-26 22:42:39 +0530

    custom design with swiftUI

---
+++ Hike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,36 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by Ganesh Prasad on 26/08/23.
+//
+
+import SwiftUI
+
+struct GradientButton: ButtonStyle {    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Nil Coalescing operator
+                // Condition ? A : B
+                configuration.isPressed ?
+                // A: When User pressed the Button
+                LinearGradient(
+                    colors: [.customGrayMedium, .customGrayLight],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                :
+                // B: When the Button is not pressed
+                LinearGradient(
+                    colors: [.customGrayLight, .customGrayMedium],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .cornerRadius(40)
+    }
+}
