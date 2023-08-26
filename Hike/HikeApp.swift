d0b3652015ab89571b17060ae6465b60476d3ea2
ganesh8prasad1991 <ganesh8prasad1991@gmail.com>
2023-08-26 17:12:47 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ganesh Prasad on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
